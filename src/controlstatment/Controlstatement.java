86c2ddb2037a68743869e307e3a284b80c035a2c
prashant <prashantpatil0509@gmail.com>
2023-04-16 23:04:47 +0530

    xyz123

---
+++ src/controlstatment/Controlstatement.java
@@ -0,0 +1,16 @@
+package controlstatment;
+
+public class Controlstatement {
+	
+	String name;
+	//constructor can accept arguments 
+	
+   Controlstatement(int a){
+
+	   name=null;
+	   System.out.println(a+" "+name); 
+   }
+	public static void main(String[] args) {
+	Controlstatement v1= new Controlstatement(10);
+
+	}}
